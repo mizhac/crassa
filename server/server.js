ba7548251f629037a8e78cdebe10f7fc41c359bf
mizhac <cristhian@krowdy.co>
2020-01-17 10:30:25 -0500

    [update] adding crassa logger

--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ import { existsSync } from 'fs'
 import express from 'express'
 import morgan from 'morgan'
 import path from 'path'
+import winston from 'winston'
 
 import index from './routes/index'
 
 import { appServer, appBuild } from '../src/paths'
-
 // Create our express app (using the port optionally specified)
 const app = express()
 const PORT = process.env.REACT_APP_PORT_SERVER || process.env.PORT || 5000
@@ -18,12 +18,37 @@ const HOST = process.env.REACT_APP_HOST_SERVER || '0.0.0.0'
 const configExpress = appServer + '/configExpress.js'
 const hasConfigExpress = existsSync(configExpress)
 
+const logger = new winston.createLogger({
+  transports: [
+    new winston.transports.File({
+      filename        : './service-tracking.log',
+      level           : 'verbose',
+      json            : false,
+      handleExceptions: true,
+      maxFiles        : 7,
+      prettyPrint     : object => { return JSON.stringify(object) }
+    }),
+    new winston.transports.Console({
+      level           : 'verbose',
+      handleExceptions: true,
+      json            : false,
+      colorize        : true,
+      prettyPrint     : object => { return JSON.stringify(object) }
+    })
+  ]
+})
+
 // Compress, parse, and log
 app.use(compression())
 app.use(cookieParser())
-app.use(bodyParser.json(process.env.BODY_PARSER_LIMIT ? { limit: process.env.BODY_PARSER_LIMIT } : {} ))
+app.use(bodyParser.json(process.env.BODY_PARSER_LIMIT ? { limit: process.env.BODY_PARSER_LIMIT } : {}))
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(morgan('dev'))
+app.use(morgan('[:date[clf]] :method :url HTTP/:http-version :status :res[content-length] - :response-time ms', {
+  stream: {
+    write: message => logger.info(message.trim())
+  }
+}))
+
 app.disable('x-powered-by')
 const http = hasConfigExpress ? require(configExpress).default(app) : app
 
